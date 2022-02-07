4b10daf89e9eee8b0ed598704baec7b9c8b80ae0
Nazar Beiar <nzbeiar@gmail.com>
2022-02-07 17:57:36 +0100

    Higher order functions implemented

--- Chapter 4 - Higher Order Functions.js
+++ Chapter 4 - Higher Order Functions.js
@@ -1,1 +1,34 @@
-const
+const mapper = (f, arr) => {
+    let mapped = [];
+    for (let el of arr)
+    {
+        mapped.push(f(el));
+    }
+    return mapped;
+}
+
+const filter = (f, arr) => {
+    let filtered = [];
+    for (let el of arr)
+    {
+        if (f(el))
+        {
+            filtered.push(el);
+        }
+    }
+    return filtered;
+}
+
+const reducer = (arr, f) => {
+    let acc = arr[0];
+    for (let i = 1; i < arr.length; i++)
+    {
+        acc = f(acc,arr[i]);
+    }
+    return acc;
+}
+
+const flatten = (arr) => {
+    return arr.reduce((a,b) => a.concat(b));
+}
+

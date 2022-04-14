3aaec7f31334a38b7ded982d6d6b2d071b215bb3
Nazar Beiar <nzbeiar@gmail.com>
2022-04-14 23:03:15 +0200

    Finished chapter exercises

--- Chapter 7 - Bugs and Errors.js
+++ Chapter 7 - Bugs and Errors.js
@@ -27,4 +27,49 @@ const reliableMultiply = (a,b) => {
 
 }
 
-console.log(reliableMultiply(8, 8));
+//console.log(reliableMultiply(8, 8));
+
+const box = {
+    locked: true,
+    unlock() { this.locked = false; },
+    lock() { this.locked = true;},
+    _content: [],
+    get content() {
+        if (this.locked) throw new Error("Locked!");
+        return this._content;
+    }
+};
+
+const withBoxUnlocked = (func) => {
+    if (box.locked === true) {
+        box.unlock();
+        try {
+            return func(3);
+        }
+        catch (err)
+        {
+            console.log("Something went wrong");
+        }
+        finally {
+            box.lock()
+        }
+    }
+    else
+    {
+        return func(3);
+    }
+}
+
+withBoxUnlocked(function() {
+    box.content.push("gold piece");
+});
+
+try {
+    withBoxUnlocked(function() {
+        throw new Error("Pirates on the horizon! Abort!");
+    });
+} catch (e) {
+    console.log("Error raised: " + e);
+}
+console.log(box.locked);
+// → true

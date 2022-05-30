050eb0354b43947d0563ca562982c27d8e7a8d1a
Nazar Beiar <nzbeiar@gmail.com>
2022-05-30 17:32:28 +0200

    Exercises started

--- Chapter-8-Regular-Expressions.js
+++ Chapter-8-Regular-Expressions.js
@@ -1,1 +1,37 @@
-na
+console.log(verify(/ca[tr]?/, ["my car", "bad cats"], ["camper", "high art"]));
+
+verify(/.../,
+    ["pop culture", "mad props"],
+    ["plop", "prrrop"]);
+
+verify(/.../,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
+
+verify(/.../,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+
+verify(/.../,
+    ["bad punctuation ."],
+    ["escape the period"]);
+
+verify(/.../,
+    ["Siebentausenddreihundertzweiundzwanzig"],
+    ["no", "three small words"]);
+
+verify(/.../,
+    ["red platypus", "wobbling nest"],
+    ["earth bed", "learning ape", "BEET"]);
+
+
+function verify(regexp, yes, no) {
+    // Ignore unfinished exercises
+    if (regexp.source === "...") return;
+    for (let str of yes) if (!regexp.test(str)) {
+        console.log(`Failure to match '${str}'`);
+    }
+    for (let str of no) if (regexp.test(str)) {
+        console.log(`Unexpected match for '${str}'`);
+    }
+}
